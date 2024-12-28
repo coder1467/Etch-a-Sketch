23dd6432ad767f49f8f754c71ca6b12097e330dd
Ahmed <134275679+coder1467@users.noreply.github.com>
2024-12-28 19:13:16 +0500

    Completed Script

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const sizeBtn = document.querySelector('.size');
 const resetBtn = document.querySelector('.reset');
-let gridSize = 64;
+const grid = document.querySelector('.grid');
+const color = document.querySelector('input');
+let gridSize = 12;
 
-
-sizeBtn.addEventListener('click', () => {
+sizeBtn.onclick = () => {
     gridSize = prompt('Enter Size of Grid: ');
     makeGrid();
-});
+};
+
+resetBtn.onclick = () => {
+    makeGrid();
+}
 
 function makeGrid() {
+    grid.innerHTML = '';
 
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.width = `${500 / gridSize}px`;
+        cell.style.height = `${500 / gridSize}px`;
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = color.value;
+        })
+        grid.appendChild(cell);
+    }
 }
+
+makeGrid();
